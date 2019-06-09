009f415ee5ef7526eb9f75c3144af9d8f20b05bd
nellaG <seirios0107@gmail.com>
2019-06-09 19:38:04 +0900

    make hashable and enable to set

--- python/vector.py
+++ python/vector.py
@@ -5,11 +5,19 @@ class Vector2d:
     typecode = 'd'
 
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.__x = float(x)
+        self.__y = float(y)
 
     def __iter__(self):
         return (i for i in (self.x, self.y))
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     def __repr__(self):
         class_name = type(self).__name__
@@ -31,12 +39,29 @@ class Vector2d:
     def __bool__(self):
         return bool(abs(self))
 
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+
     @classmethod
     def frombytes(cls, octets):
         """ frombytes """
         typecode = chr(octets[0])
         memv = memoryview(octets[1:]).cast(typecode)
         return cls(*memv)
+
+    def __format__(self, fmt_spec=''):
+        if fmt_spec.endswith('p'):
+            fmt_spec = fmt_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}, {}>'
+        else:
+            coords = self
+            outer_fmt = '({}, {})'
+        components = (format(c, fmt_spec) for c in coords)
+        return outer_fmt.format(*components)
+
+    def angle(self):
+        return math.atan2(self.y, self.x)
 
 
 if __name__ == '__main__':
