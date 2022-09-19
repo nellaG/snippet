b4a20d7d5f2c4bb2707bf5c7982cd0c20d723f59
nellaG <seirios0107@gmail.com>
2022-09-19 16:50:00 +0900

    add go web example: use custom router

---
+++ go/web_hello_world.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello world ~ %s\n", r.URL.Path)
+	})
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+
+	})
+
+	http.ListenAndServe(":80", r)
+}
