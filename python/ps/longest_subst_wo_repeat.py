d0f7ebea9ef7eae3f53447d201528d10d713e4c5
nellaG <seirios0107@gmail.com>
2022-09-25 01:54:25 +0900

    change brute force to sliding window

--- python/ps/longest_subst_wo_repeat.py
+++ python/ps/longest_subst_wo_repeat.py
@@ -1,16 +1,24 @@
 """ https://leetcode.com/problems/longest-substring-without-repeating-characters/ """
 
+from collections import Counter
+
+
 class Solution:
     def engthOfLongestSubstring(self, s: str) -> int:
         length = len(s)
         res = 0
-        for i in range(length):
-            for j in range(i, length):
-                if self.is_unique(s[i:j + 1]):
-                    res = max(res, j - i + 1)
+        ch = Counter()
+        lidx = ridx = 0
+
+        while ridx < length:
+            r = s[ridx]
+            ch[r] += 1
+
+            while ch[r] > 1:  # move window
+                l = s[lidx]
+                ch[l] -= 1
+                lidx += 1
+            res = max(res, ridx + 1 - lidx)
+            ridx += 1
 
         return res
-
-    def is_unique(self, st):
-        stlist = list(st)
-        return list(set(stlist)) == stlist
